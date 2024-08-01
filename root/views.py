5125ecdeb8ae55cd658f968bc9354d4d7f1616fb
rezamohseni <rezamohseni@gmail.com>
2024-08-02 00:31:41 +0330

    class base

--- root/views.py
+++ root/views.py
@@ -1,19 +1,34 @@
+from typing import Any
+from django.http.request import HttpRequest as HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render , redirect
 from services.models import SpecialService
 from .models import FrequentlyQuestions , ContactUs
 from services.models import Team
 from .forms   import ContactUSForm
 from django.contrib import messages
+from django.views.generic import TemplateView
 
-def home(request):
+# def home(request):
 
-    context = {
-        'specials': SpecialService.objects.filter(status=True),
-        'team' : Team.objects.filter(status=True),
-        'questions': FrequentlyQuestions.objects.filter(status=True)[::-1],
-        }
+#     context = {
+#         'specials': SpecialService.objects.filter(status=True),
+#         'team' : Team.objects.filter(status=True),
+#         'questions': FrequentlyQuestions.objects.filter(status=True)[::-1],
+#         }
     
-    return render(request, 'root/index.html', context=context)
+#     return render(request, 'root/index.html', context=context)
+
+class HomeView(TemplateView):
+    template_name = 'root/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['team'] = Team.objects.all()
+        context ['questions'] = FrequentlyQuestions.objects.all()
+        context ['specials'] = SpecialService.objects.all()
+        return context 
+
 
 def contact(request):
     if request.method == 'POST':
@@ -33,9 +48,16 @@ def contact(request):
         form = ContactUSForm()
         return render(request, "root/contact.html" , context={'form' : form})
 
-def about(request):
-    context = {
-        'team' : Team.objects.filter(status=True),
-        }
-    return render(request, "root/about.html", context=context)
-
+# def about(request):
+#     context = {
+#         'team' : Team.objects.filter(status=True),
+#         }
+#     return render(request, "root/about.html", context=context)
+class AboutView(TemplateView):
+    template_name = "root/about.html"
+    
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context ['team'] = Team.objects.all()
+        return context 
+        
--- services/views.py
+++ services/views.py
@@ -1,60 +1,98 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render , get_object_or_404 , redirect
 from .models import SpecialService, Team, Skill , Category , Option , Service , Comment
 from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
 from .forms import CommentForm
 from django.contrib import messages
+from django.views.generic import ListView , DetailView
 
-def services(request, **kwargs):
-    if kwargs.get('category'):
-        all_service = Service.objects.filter(category__title=kwargs.get('category'))
+class ServiceView(ListView):
+    template_name = 'services/services.html'
+    context_object_name = 'services'
+    paginate_by =  2
+    def get_queryset(self):
+        if self.kwargs.get('category'):
+            all_service = Service.objects.filter(category__title=self.kwargs.get('category'))
 
-    elif request.GET.get('search') is not None:
-        all_service = Service.objects.filter(content__contains=request.GET.get('search'))
+        if self.kwargs.get('search'):
+            all_service = Service.objects.filter(content__contains=self.kwargs.get('search'))
 
-    elif kwargs.get('price'):
-        all_service = Service.objects.filter(price__lte=kwargs.get('price'))
+        if self.kwargs.get('price'):
+            all_service = Service.objects.filter(price__lte=self.kwargs.get('price'))
+        else:
+            all_service = Service.objects.filter(status = True)
 
-    else:    
-        all_service = Service.objects.filter(status=True)
+        return all_service
+
+# def services(request, **kwargs):
+#     if kwargs.get('category'):
+#         all_service = Service.objects.filter(category__title=kwargs.get('category'))
+
+#     elif request.GET.get('search') is not None:
+#         all_service = Service.objects.filter(content__contains=request.GET.get('search'))
+
+#     elif kwargs.get('price'):
+#         all_service = Service.objects.filter(price__lte=kwargs.get('price'))
+
+#     else:    
+#         all_service = Service.objects.filter(status=True)
 
 
-    all_services = Paginator(all_service,2)
+#     all_services = Paginator(all_service,2)
 
-    try:
-        page_number = request.GET.get('page')
-        all_services = all_services.get_page(page_number)
+#     try:
+#         page_number = request.GET.get('page')
+#         all_services = all_services.get_page(page_number)
 
-    except PageNotAnInteger:
-        all_services = all_services.get_page(1)
+#     except PageNotAnInteger:
+#         all_services = all_services.get_page(1)
 
-    except EmptyPage:
-        all_services = all_services.get_page(1)
+#     except EmptyPage:
+#         all_services = all_services.get_page(1)
     
 
-    context = {            
-            "services" : all_services,
-            "special_services": SpecialService.objects.filter(status=True), 
-        }
-    return render(request, 'services/services.html' , context = context)
+#     context = {            
+#             "services" : all_services,
+#             "special_services": SpecialService.objects.filter(status=True), 
+#         }
+#     return render(request, 'services/services.html' , context = context)
 
+class Service_detail_view(DetailView):
+    model = Service
+    template_name = 'services/service-details.html'
+    context_object_name = 'service_detail'
 
-def services_detail(request ,id):
-
-    try:
-        service = get_object_or_404(Service ,  id=id)
-        comment = Comment.objects.filter(product_name=service.name , status=True)
+    def get(self, request, *args , **kwargs):
+        service = self.model.objects.get(pk=kwargs.get('pk'))
         service.counted_view += 1
         service.save()
+    # def get_context_data(self,**kwargs):
 
-        context = {
-        'service_detail': service,
-        'comment' : comment
 
-        }
-        return render(request, 'services/service-details.html' , context=context)
 
-    except: 
-        return render(request, 'services/404.html' )
+
+
+
+
+# def services_detail(request ,id):
+
+#     try:
+#         service = get_object_or_404(Service ,  id=id)
+#         comment = Comment.objects.filter(product_name=service.name , status=True)
+#         service.counted_view += 1
+#         service.save()
+
+#         context = {
+#         'service_detail': service,
+#         'comment' : comment
+
+#         }
+#         return render(request, 'services/service-details.html' , context=context)
+
+#     except: 
+#         return render(request, 'services/404.html' )
 
 
 def qoute(request):
